a300af6bdbc70a30e6f3357592fea1e878e34728
Gustavo Fernandes Costa <84602100+gustavofcosta@users.noreply.github.com>
2023-01-30 01:08:51 -0300

    edit max-width

--- src/components/CardList/styled.ts
+++ src/components/CardList/styled.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  max-width: 100%;
+  max-width: 1500px;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
   gap: 20px;
