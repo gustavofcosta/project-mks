a7fa0254ca3159148700bc07ab3cab7c63e9d6df
Gustavo Fernandes Costa <84602100+gustavofcosta@users.noreply.github.com>
2023-01-30 01:30:00 -0300

    edit layout

--- src/components/CardList/styled.ts
+++ src/components/CardList/styled.ts
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  min-width: 100%;
+  max-width: 100%;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
   gap: 20px;
-  padding: 10px  200px;
+  margin: 70px 60px;
 
   @media screen and (min-width: 500px) {
     gap: 22px;
-    grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+    margin: 116px 100px;
   }
 `;
